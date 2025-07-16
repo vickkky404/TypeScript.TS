d9b8e9b21b88aa31848b5123cfcdf7eab253bda6
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-16 14:23:45 +0530

    Add files via upload

---
+++ CrashC.ts
@@ -0,0 +1,10 @@
+let names: String = "Vicky";
+let age: number = 25;
+let isActive: boolean = true;
+
+// any allows anything
+let something: any = "Hello";
+something = 42;
+
+let city = "Odisha" // string
+console.log(names , age , isActive , something , city)
