133df8b62bd48bc5c6fa2ea0121f06fcd8161a11
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-16 14:25:25 +0530

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,8 @@
+function myFunction() {
+    var a = 15;
+    let b = 16;
+    console.log(a); // Move inside function
+    console.log(b); // Move inside function
+}
+
+myFunction(); // Call the function 
