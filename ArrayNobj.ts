45f62a7d5230b4a5aad69070b687207d62b136ac
nalinikant bastia <106691792+vickkky404@users.noreply.github.com>
2025-07-16 14:23:14 +0530

    Add files via upload

---
+++ ArrayNobj.ts
@@ -0,0 +1,10 @@
+let numbers: number[] = [1, 2, 3];
+let namez: string[] = ["Alice", "Bob"];
+
+// object with type
+let person: { name: string; age: number } = {
+  name: "Vickky",
+  age: 25
+};
+
+console.log(person.name);
