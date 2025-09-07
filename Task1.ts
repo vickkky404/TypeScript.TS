e46e84a2bbd734228538e2a06f594f8c9530baf1
Nalinikant Bastia <106691792+vickkky404@users.noreply.github.com>
2025-09-07 21:09:39 +0530

    Add files via upload

---
+++ Task1.ts
@@ -0,0 +1,14 @@
+// create varibale for = your name , your age , whether you are a student or not , Arrays
+
+let myName: string = "Nalinikant";
+let myAge: number = 21;
+let isStudent: boolean = true ;
+let favLanguage: string[] = ["Java" , "C++" , "Python"];
+
+console.log("myName" , myName);
+console.log("myAge" ,   myAge);
+console.log("isStudent" , isStudent);
+console.log("favLanguage" , favLanguage);
+
+
+
